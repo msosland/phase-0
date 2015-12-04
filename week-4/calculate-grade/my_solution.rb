816443a7ce3495ac0fd033f4f6a6bcb229d11d90
Marshall Sosland <marshall.sosland@gmail.com>
2015-12-04 09:35:17 -0600

    add all files for 4.5 pair sesh

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,20 @@
+
+# Calculate a Grade
+
+# I worked on this challenge with Katie Meyer!
+
+
+# Your Solution Below
+def get_grade(num)
+  if num >= 90
+    return "A"
+  elsif num >= 80
+    return "B"
+  elsif num >= 70
+    return "C"
+  elsif num >= 60
+    return "D"
+  else
+    return "F"
+  end
+end
