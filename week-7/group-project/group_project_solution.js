697c5081cb4f7dbc345be0fe615cb00a594cb50a
Marshall Sosland <marshall.sosland@gmail.com>
2015-12-28 18:39:26 -0500

    add test file and user stories for group project

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,7 @@
+//User stories by Marshall Sosland
+
+// As a user, I want to have a function called sum that can iterate through an array and add all the elements of the array.
+
+// As a user, I want to have another function called mean that can find the average of all the numbers in an array.
+
+// As a user, I also want to have a function called median that can find the median of all the numbers in an array. I really want it to work for both arrays with an odd number of elements and those with an even number of elements!
